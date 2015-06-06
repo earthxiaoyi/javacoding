32f7f9f988f8962fb1560e53809061482788cc10
JM <JM@JM-PC>
2015-06-06 21:40:37 +0800

    增加rabbitmq使用示例

---
+++ src/main/java/com/javacoding/rabbitmq/demo/Client.java
@@ -0,0 +1,20 @@
+package com.javacoding.rabbitmq.demo;
+
+import java.util.HashMap;
+
+public class Client {
+	public static void main(String[] args) throws Exception {
+		QueueConsumer consumer = new QueueConsumer("queue");
+		Thread consumerThread = new Thread(consumer);
+		consumerThread.start();
+
+		Producer producer = new Producer("queue");
+
+		for (int i = 0; i < 100000; i++) {
+			HashMap message = new HashMap();
+			message.put("message number", i);
+			producer.sendMessage(message);
+			System.out.println("Message Number " + i + " sent.");
+		}
+	}
+}
---
+++ src/main/java/com/javacoding/rabbitmq/demo/Producer.java
@@ -0,0 +1,17 @@
+package com.javacoding.rabbitmq.demo;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import org.apache.commons.lang.SerializationUtils;
+
+public class Producer extends EndPoint{
+     
+    public Producer(String endPointName) throws IOException{
+        super(endPointName);
+    }
+ 
+    public void sendMessage(Serializable object) throws IOException {
+        channel.basicPublish("",endPointName, null, SerializationUtils.serialize(object));
+    }  
+}
